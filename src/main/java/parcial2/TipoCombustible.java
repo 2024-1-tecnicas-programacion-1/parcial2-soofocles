3f5589cba49f5002637cc6ea1b89e348f49a1716
invitado <anasfajardol@gmail.com>
2024-06-06 09:42:01 -0500

    Tipo combustible y motocicleta
    Se realizaron las respectivas clases

--- src/main/java/parcial2/TipoCombustible.java
+++ src/main/java/parcial2/TipoCombustible.java
@@ -1,5 +1,7 @@
 package parcial2;
 
 public enum TipoCombustible {
-    // TODO: Tu código va aquí
+    GASOLINA,
+    ELECTRICO,
+    HIBRIDO,
 }
--- src/main/java/parcial2/TipoMotocicleta.java
+++ src/main/java/parcial2/TipoMotocicleta.java
@@ -1,5 +1,11 @@
 package parcial2;
 
 public enum TipoMotocicleta {
-    // TODO: Tu código va aquí
+    DEPORTIVA,
+    TOURING,
+    NAKED,
+    CAFE_RACER,
+    CRUISER,
+    SCOOTER,
+    ENDURO,
 }
