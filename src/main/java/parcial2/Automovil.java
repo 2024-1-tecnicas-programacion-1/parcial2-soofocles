590f3ebb5b05e1864e14b8741fc8adeafb52b294
invitado <anasfajardol@gmail.com>
2024-06-06 10:02:21 -0500

    Automóvil y vehículo

--- src/main/java/parcial2/Automovil.java
+++ src/main/java/parcial2/Automovil.java
@@ -1,5 +1,17 @@
 package parcial2;
 
-public class Automovil {
-    // TODO: Tu código va aquí
+class Automovil extends Vehiculo {
+    int numeroPuertas;
+    TipoCombustible tipoCombustible;
+
+    public Automovil(String marca, String modelo, int año, int numeroPuertas, TipoCombustible tipoCombustible) {
+        super(marca, modelo, año);
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    @Override
+    public String imprimirInformacion() {
+        return super.imprimirInformacion() + ", " + numeroPuertas + " puertas, combustión a " + tipoCombustible;
+    }
 }
--- src/main/java/parcial2/Vehiculo.java
+++ src/main/java/parcial2/Vehiculo.java
@@ -1,5 +1,17 @@
 package parcial2;
 
 public class Vehiculo {
-    // TODO: Tu código va aquí
+    String marca;
+    String modelo;
+    int año;
+
+    public Vehiculo(String marca, String modelo, int año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
+    public String imprimirInformacion() {
+        return marca + " " + modelo + " " + año;
+    }
 }
